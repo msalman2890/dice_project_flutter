597644d7891cc169ee6f85b406a76e163763171c
Muhammad Salman <muhammadsalman@folio3.com>
2023-04-27 10:49:30 +0500

    complete implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,17 +16,64 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String dice1Image = "assets/dice1.png";
+
+  String dice2Image = "assets/dice1.png";
+
+  void rollDice() {
+    int randomNumber = Random().nextInt(6) + 1;
+    int randomNumber2 = Random().nextInt(6) + 1;
+    setState(() {
+      dice1Image = "assets/dice$randomNumber.png";
+      dice2Image = "assets/dice$randomNumber2.png";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Dice Roll Project"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                dice1Image,
+                height: 120,
+                width: 120,
+              ),
+              Image.asset(
+                dice2Image,
+                height: 120,
+                width: 120,
+              )
+            ],
+          ),
+          ElevatedButton(
+            onPressed: rollDice,
+            child: Text(
+              "Roll Dice",
+              style: TextStyle(fontSize: 18),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
-
